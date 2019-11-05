77d37621e4f8a43b9c33bc3a45f821d829348bfa
ddkanungo-gmail <ddkanungo@gmail.com>
2019-11-05 04:10:00 +0000

    #1  Changing Label

--- src/main/java/com/deep/todo/validation/Scratchpad.java
+++ src/main/java/com/deep/todo/validation/Scratchpad.java
@@ -3,6 +3,6 @@ package com.deep.todo.validation;
 public class Scratchpad {
 
     public  void method() {
-            System.out.println("Hello World!! ");
+            System.out.println("Hello GitLab!! ");
     }
 }
